2a65663b08cb9b5af70dce0fa502fa6fbae8c8cb
Jarek <z.jarek92@gmail.com>
2018-08-20 13:38:15 +0200

    exact in router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-dom'
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Router>
 
+          <Route path={'/'} exact={true} component={Hello} />
+
+        </Router>
       </div>
     )
   }
 }
 
-export default App;
+const Hello = function (props) {
+  return <p>Hello</p>
+}
+
+export default App
