5c8d34fe2f847c973d1edcb4cc49c305a530e4dc
Jarek <z.jarek92@gmail.com>
2018-08-20 16:25:53 +0200

    users component

--- src/Users.js
+++ src/Users.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const Users = (props) => (
-    <div>
-        Users
-  </div>
-)
+class Users extends Component {
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => ({})
 
 const mapDispatchToProps = dispatch => ({})
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Users)
